6c55ab51fc6fddf3def6c5f032a5ca7ecf8c827e
dmitriiaristov <mitya.aristov@yandex.ru>
2025-04-27 14:00:50 +0400

    amend HelloWorld.java to dynamic routes

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -13,6 +13,24 @@ public class HelloWorld {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name + "!");
         });
+        app.get("/courses/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("id"));
+        });
+        app.get("/users/{id}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam(("id")));
+        });
+
+        app.get("/courses/{courseId}/lessons/{id}", ctx -> {
+            var courseId = ctx.pathParam("courseId");
+            var lessonId = ctx.pathParam("id");
+            ctx.result("Course ID: " + courseId + " Lesson ID: " + lessonId);
+        });
+
+        app.get("users/{id}/post/{postId}", ctx -> {
+            var userId = ctx.pathParam("id");
+            var postId = ctx.pathParam("postId");
+            ctx.result("user ID: " + userId + " post ID: " + postId);
+        });
         app.start(7070);
     }
 }
